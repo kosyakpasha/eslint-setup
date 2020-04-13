f446f4b7b60b5d1492c5774f9d48e1435424a64c
pavlo_kosiak <pavlo_kosiak@epam.com>
2020-04-13 16:29:13 +0300

    - add new prop

--- bad-code.js
+++ bad-code.js
@@ -1,122 +1,129 @@
-var count = 1;
+let count = 1;
 if (true) {
-    count += 1;
+  count += 1;
 }
 
 
 const atom = {
-    value: 1,
+  value: 1,
 
-    addValue: function (value) {
-        return atom.value + value;
-    },
+  addValue(value) {
+    return atom.value + value;
+  },
 };
 
 const bad = {
-    'foo': 3,
-    'bar': 4,
-    'data-blah': 5,
+  foo: 3,
+  bar: 4,
+  'data-blah': 5,
+  'data-blah2': 5,
 };
 
-const name = "Capt. Janeway";
+const name = 'Capt. Janeway';
 
 function sayHi(name) {
-    return 'How are you, ' + name + '?';
+  return `How are you, ${  name  }?`;
 }
 
-const f = function(){};
-const g = function (){};
+const f = function() {};
+const g = function() {};
 const h = function() {};
 
-function test(){
-    console.log('test');
+function test() {
+  console.log('test');
 }
 
-if(isJedi) {
-    fight ();
+if (isJedi) {
+  fight ();
 }
 
 new (Function.prototype.bind.apply(Date, [null, 2016, 8, 5]));
 
-function foo2(bar,
-              baz,
-              quux) {
-    // ...
+function foo2(
+  bar,
+  baz,
+  quux
+) {
+  // ...
 }
 
-[1, 2, 3].map(function (x) {
-    const y = x + 1;
-    return x * y;
+[1, 2, 3].map((x) => {
+  const y = x + 1;
+  return x * y;
 });
 
-foo2(() => bool = true);
+foo2(() => {
+  return bool = true;
+});
 
-const foo3
-    = 'superLongLongLongLongLongLongLongLongString';
+const foo3    = 'superLongLongLongLongLongLongLongLongString';
 
 if ([0] && []) {
-    // true
-    // an array (even an empty one) is an object, objects will evaluate to true
+  // true
+  // an array (even an empty one) is an object, objects will evaluate to true
 }
 
 const foo5 = a ? a : b;
-const bar10 = c ? true : false;
-const baz11 = c ? false : true;
+const bar10 = !!c;
+const baz11 = !c;
 
 function cats() {
-    if (x) {
-        return x;
-    } else if (y) {
-        return y;
-    }
+  if (x) {
+    return x;
+  } if (y) {
+    return y;
+  }
 }
 
-//is current tab
+// is current tab
 const active = true;
 
-if(isJedi) {
-    fight ();
+if (isJedi) {
+  fight ();
 }
 
-const x=y+5;
+const x = y + 5;
 
-const leds = stage.selectAll('.led').data(data).enter().append('svg:svg').classed('led', true)
-    .attr('width', (radius + margin) * 2).append('svg:g')
-    .attr('transform', `translate(${radius + margin},${radius + margin})`)
-    .call(tron.led);
+const leds = stage.selectAll('.led').data(data)
+  .enter()
+  .append('svg:svg')
+  .classed('led', true)
+  .attr('width', (radius + margin) * 2)
+  .append('svg:g')
+  .attr('transform', `translate(${radius + margin},${radius + margin})`)
+  .call(tron.led);
 
 function bar() {
-
-    console.log(foo);
-
+  console.log(foo);
 }
 
-function bar2( foo ) {
-    return foo;
+function bar2(foo) {
+  return foo;
 }
 
-const foo1 = [ 1, 2, 3 ];
+const foo1 = [1, 2, 3];
 console.log(foo[ 0 ]);
 
-const baz = {clark: 'kent'};
+const baz = { clark: 'kent' };
 
 const story = [
-    once
-    , upon
-    , aTime
+  once,
+  upon,
+  aTime,
 ];
 
-function foo() { return false; }
+function foo() {
+  return false; 
+}
 
 if (test) {
-    thing1();
-    thing2();
-}
-else {
-    thing3();
+  thing1();
+  thing2();
+} else {
+  thing3();
 }
 
 const hero = {
-    firstName: 'Dana',
-    lastName: 'Scully'
+  firstName: 'Dana',
+  lastName: 'Scully',
 };
